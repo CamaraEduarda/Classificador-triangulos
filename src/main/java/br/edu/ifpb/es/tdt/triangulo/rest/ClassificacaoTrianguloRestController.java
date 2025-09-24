44f162c7a2fc87ef8000e511bb99243689a98e23
Maria Eduarda Câmara <144015917+CamaraEduarda@users.noreply.github.com>
2025-09-24 22:45:36 +0000

    atividade dos triângulos parte 1

--- src/main/java/br/edu/ifpb/es/tdt/triangulo/rest/ClassificacaoTrianguloRestController.java
+++ src/main/java/br/edu/ifpb/es/tdt/triangulo/rest/ClassificacaoTrianguloRestController.java
@@ -2,6 +2,7 @@ package br.edu.ifpb.es.tdt.triangulo.rest;
 
 
 import br.edu.ifpb.es.tdt.triangulo.model.ClassificacaoTriangulo;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,33 @@ public class ClassificacaoTrianguloRestController implements ClassificacaoTriang
     public ResponseEntity<ClassificacaoTriangulo> getClassificacaoTriangulo(@RequestParam String lado1,
                                                                             @RequestParam String lado2,
                                                                             @RequestParam String lado3) {
-        return new ResponseEntity<>(ClassificacaoTriangulo.EQUILÁTERO, HttpStatus.OK);
+                                                                            
+        int valor1 = Integer.parseInt(lado1);
+        int valor2 = Integer.parseInt(lado2);
+        int valor3 = Integer.parseInt(lado3);
+        
+        if (!((valor1 + valor2 > valor3) && (valor1 + valor3 > valor2) && (valor2 + valor3 > valor1))) {
+            return new ResponseEntity<>(ClassificacaoTriangulo.INVÁLIDO, HttpStatus.BAD_REQUEST);
+        }
+
+        if ((valor1 == valor2) && (valor2 == valor3) && (valor1 == valor3)) {
+            return new ResponseEntity<>(ClassificacaoTriangulo.EQUILÁTERO, HttpStatus.OK);
+        }
+
+        if ((valor1 == valor2) || (valor2 == valor3) || (valor1 == valor3)) {
+            return new ResponseEntity<>(ClassificacaoTriangulo.ISÓSCELES, HttpStatus.OK);
+        }
+
+        if ((valor1 != valor2) && (valor2 != valor3) && (valor1 != valor3)) {
+            return new ResponseEntity<>(ClassificacaoTriangulo.ESCALENO, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(ClassificacaoTriangulo.INVÁLIDO, HttpStatus.BAD_REQUEST);
     }
 
+
+    
+
 }
+
+
